9c33a477fb87dfad4d57262d6e52d16019b40cb9
Veehansystum <145607257+Veehansystum@users.noreply.github.com>
2026-03-13 16:28:57 +0530

    Update utils.js

--- myapp/src/utils.js
+++ myapp/src/utils.js
@@ -3,4 +3,4 @@ import { toast } from "react-toastify";
 export const notify = (message,type)=>{
     toast[type](message);
 }
-export const API_URL = 'https://mern-task-manager-app-deploy-br299gdkq.vercel.app';
+export const API_URL = 'https://mern-task-manager-app-deploy-apiii.vercel.app';
